215c9c1f7836307d40e352bc4b98f38396f3edf4
Yangming Zhang <zhangseammm@gmail.com>
2025-05-20 22:08:06 -0500

    build up framework

--- static/js/index.js
+++ static/js/index.js
@@ -78,4 +78,20 @@ $(document).ready(function() {
 })
 
 // video-comparison-slider
+const video = document.querySelector(".video");
+const toggleButton = document.querySelector(".toggleButton");
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+function updateToggleButton() {
+  toggleButton.innerHTML = video.paused ? "►" : "❚❚";
+}
+toggleButton.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateToggleButton);
+video.addEventListener("pause", updateToggleButton);
 
